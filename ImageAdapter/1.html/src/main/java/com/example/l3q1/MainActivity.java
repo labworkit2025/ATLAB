f09e8666fb095e47b9d2e74612a5b371179f920c
labworkit2025 <160770870+labworkit2025@users.noreply.github.com>
2024-02-22 00:36:53 +0530

    Add files via upload

---
+++ ImageAdapter/1.html/src/main/java/com/example/l3q1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.l3q1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Image " + (position + 1), Toast.LENGTH_SHORT).show();
+
+                int selectedImageId = ((ImageAdapter) parent.getAdapter()).getItem(position);
+
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setImageResource(selectedImageId);
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setView(imageView);
+                toast.show();
+            }
+        });
+
+    }
+}
